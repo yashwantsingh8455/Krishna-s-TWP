def77ae240429cae25b897133e76f40b0008ce94
Yashwant <143935236+yashwantsingh8455@users.noreply.github.com>
2026-05-12 11:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -275,8 +275,6 @@ client.login(DISCORD_TOKEN).catch(err => console.error("Discord Login Error:", e
 
 
 
-const path = require('path');
-
 // Public folder ko static banayein
 app.use(express.static(path.join(__dirname, 'public')));
 
